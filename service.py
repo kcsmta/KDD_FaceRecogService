765399daf77513ab65552e66a52d9f4d68c3a6c9
trandat <tranmanhdathsgs@gmail.com>
2020-05-07 13:40:45 +0700

    add get_all to view all ID

--- service.py
+++ service.py
@@ -3,7 +3,7 @@ import cv2
 import flask
 from flask import request, jsonify
 from werkzeug.utils import secure_filename
-from face_encoder.support import  predict
+from face_encoder.support import predict
 from utils import *
 from waitress import serve
 
@@ -19,32 +19,67 @@ app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024  # max 200mb in a request
 app.config["DEBUG"] = True  # for debug
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-
+@app.route('/get_all', methods=['GET'])
+def get_all():
+    global face_db_path
+    list_subfolders_with_paths = [f.name for f in os.scandir(face_db_path) if
+                                  f.is_dir()]
+    result = {}
+    for subfolder in list_subfolders_with_paths:
+        result[str(subfolder).split('_')[0]] = str(subfolder).split('_')[1]
+    return jsonify(result)
 @app.route('/add_person', methods=['POST'])
 def add_person():
     global ID, list_id
     if request.method == 'POST':
         files = request.files.getlist('file')
-        if request.files['file'].filename=='':
+        if request.files['file'].filename == '':
             return jsonify(message='No file selected for uploading'), 400
         elif 'name' not in request.form:
             return jsonify(message='Bad request! No name exist!'), 400
-        elif request.form['name']=='':
+        elif request.form['name'] == '':
             return jsonify(message='Bad request! Name is empty!'), 400
         else:
             name = request.form['name']
             folder_new_person = os.path.join(face_db_path, str(ID) + '_' + name)
             ID = ID + 1
             os.makedirs(folder_new_person)
+            result = {}
             for file in files:
                 if file and allowed_file(file.filename):
                     filename = secure_filename(file.filename)
                     path_to_file = os.path.join(
                             folder_new_person, filename)
                     print(path_to_file)
-                    file.save(path_to_file)
-            list_id.append(ID)
-            return jsonify(message='upload success'), 200
+                    path_temp = os.path.join(
+                            temp_path, filename)
+                    if os.path.isfile(path_temp):
+                        os.remove(path_temp)
+                    file.save(path_temp)
+                    img = cv2.imread(path_temp)
+                    faces, _ = mtcnn_detector.detect(img)
+                    if len(faces) != 1:
+                        result[file.filename] = 'Image must have only 1 face!'
+                        # os.remove(path_temp)
+                    else:
+                        x1, y1, x2, y2 = int(faces[0][0]), int(
+                                faces[0][1]), int(
+                                faces[0][2]), int(faces[0][3])
+                        # print('face size ', y2-y1, x2-x1)
+                        if y2 - y1 < 161 or x2 - x1 < 161:
+                            result[file.filename] = 'Need bigger face in image!'
+                            # os.remove(path_temp)
+                        else:
+                            face_image = img[y1:y2, x1:x2]
+                            cv2.imwrite(path_to_file, face_image)
+                            result[file.filename] = 'Success'
+                    os.remove(path_temp)
+            if len(os.listdir(folder_new_person)) != 0:
+                list_id.append(ID)
+            else:
+                ID = ID -1
+                os.rmdir(folder_new_person)
+            return jsonify(result), 200
 
 
 @app.route('/del_person', methods=['GET'])
@@ -60,7 +95,8 @@ def del_person():
             for folder in os.scandir(face_db_path):
                 if int(str(folder.name).split('_')[0]) == int(id_person):
                     shutil.rmtree(folder)
-                    return jsonify(message='delete on id_person ' + id_person), 200
+                    return jsonify(
+                        message='delete on id_person ' + id_person), 200
         else:
             return jsonify(message='id does not exist'), 400
 
@@ -72,7 +108,7 @@ def predict_img():
         files = request.files.getlist('file')
         # print(request.files['file'].filename)
         # thresh = request.form['thresh']
-        if request.files['file'].filename=='':
+        if request.files['file'].filename == '':
             return jsonify(message='No file selected for predict'), 400
         else:
             result = {}
@@ -96,6 +132,8 @@ def predict_img():
                                               VERIFICATION_THRESHOLD=0.5)
                     result[filename] = len(names), names, sims
             return jsonify(str(result)), 200
+
+
 if __name__ == '__main__':
     create_folder(face_db_path, predict_path, temp_path)
     mtcnn_detector, facenet, face_db = init_recognizer()
